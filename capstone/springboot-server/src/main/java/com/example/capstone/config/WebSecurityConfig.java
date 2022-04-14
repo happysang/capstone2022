4f37569989f902c87b2b3e860ee9b9ff81d2d0c9
khs <69028828+happysang@users.noreply.github.com>
2022-04-14 17:00:22 +0900

    Update WebSecurityConfig.java

--- capstone/springboot-server/src/main/java/com/example/capstone/config/WebSecurityConfig.java
+++ capstone/springboot-server/src/main/java/com/example/capstone/config/WebSecurityConfig.java
@@ -29,9 +29,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
 				.authorizeRequests() // /와 /auth/** 와 /getFlask 경로는 인증 안해도 됨.
-						.antMatchers("/", "/auth/**", "/getFlask", "/uploader").permitAll()
-				.anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
-						.authenticated();
+						.antMatchers("/", "/auth/**", "/getFlask", "/uploader").permitAll();
+//				.anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+//						.authenticated();
 
 		// filter 등록.
 		// 매 리퀘스트마다
