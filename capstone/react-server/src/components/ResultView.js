7ada0e391d85ecd2a88579efdd64f9020a745c28
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-05-26 19:07:26 +0900

    get id data of user

--- capstone/react-server/src/components/ResultView.js
+++ capstone/react-server/src/components/ResultView.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import MyResponsiveRadar from "./MyResponsiveRadar";
 import TinySlider from "tiny-slider-react";
 import styles from "./ResultView.module.css";
 import { API_BASE_URL } from "../service/backend-config";
 import datasets from "../result-data.json";
+import AuthContext from "../store/auth-context";
 
 const ResultView = () => {
+  const authCtx = useContext(AuthContext);
+  const { id } = authCtx;
   const [isActive, setActive] = useState("1");
   // const [data, setData] = useState("");
 
@@ -16,7 +19,7 @@ const ResultView = () => {
     typeOfScalp: localstorage.typeOfScalp,
   };
   const type = resultObj.result; // 진단 결과 타입
-  console.log('res', resultObj);
+  console.log("res", resultObj);
   console.log(type);
 
   const markHandler = (e) => {
@@ -24,20 +27,21 @@ const ResultView = () => {
     console.log(markNum);
     console.log(`${isActive === String(1) ? "isActive" : "none"}`);
     setActive(markNum);
-
   };
 
-  const obj = {};
-  const data = resultObj.typeOfScalp.map((val) => 
-     val.value
- )
+  console.log("id", id);
 
- data.forEach((val, idx) => {
-   obj[`data${idx+1}`] = val || 0;
- })
+  const obj = {
+    user_id: id,
+  };
+  const data = resultObj.typeOfScalp.map((val) => val.value);
 
- console.log(data);
- console.log(obj);
+  data.forEach((val, idx) => {
+    obj[`data${idx + 1}`] = val || 0;
+  });
+
+  console.log(data);
+  console.log(obj);
 
   useEffect(() => {
     // const data = localStorage.getItem("RESULT");
