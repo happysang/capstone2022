224ccf297ca9f0cee973afc1aa3ce9d4ebe30bef
dmswl <76807107+dmswl98@users.noreply.github.com>
2022-05-26 15:20:28 +0900

    Apply result-data.json on ResultView.js

--- capstone/react-server/src/components/MyResponsiveRadar.js
+++ capstone/react-server/src/components/MyResponsiveRadar.js
@@ -2,15 +2,15 @@ import { ResponsiveRadar } from "@nivo/radar";
 import React, { useState } from "react";
 import styles from "./MyResponsiveRadar.module.css";
 
-const MyResponsiveRadar = () => {
-  const [errorMessage, setErrorMeesage] = useState("");
+const MyResponsiveRadar = ({ data }) => {
+  // const [errorMessage, setErrorMeesage] = useState("");
 
-  const resultObj = JSON.parse(localStorage.getItem("RESULT"));
-  if (!resultObj) setErrorMeesage("에러 발생");
+  // const resultObj = JSON.parse(localStorage.getItem("RESULT"));
+  // if (!resultObj) setErrorMeesage("에러 발생");
 
-  if (errorMessage) console.log(errorMessage);
+  // if (errorMessage) console.log(errorMessage);
 
-  const resultData = resultObj.typeOfScalp.map((item) => {
+  const resultData = data.typeOfScalp.map((item) => {
     console.log(item);
     return {
       type: item.type,
@@ -20,43 +20,49 @@ const MyResponsiveRadar = () => {
   });
 
   return (
-    <div style={{ width: "600px", height: "600px" }}>
-      <ResponsiveRadar
-        data={resultData}
-        keys={["normal", "user"]}
-        indexBy="type"
-        valueFormat=">-.2f"
-        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-        borderColor={{ from: "color" }}
-        gridLabelOffset={20}
-        dotSize={10}
-        dotColor={{ theme: "background" }}
-        dotBorderWidth={2}
-        colors={{ scheme: "paired" }}
-        blendMode="multiply"
-        motionConfig="wobbly"
-        legends={[
-          {
-            anchor: "top-left",
-            direction: "column",
-            translateX: -50,
-            translateY: -40,
-            itemWidth: 80,
-            itemHeight: 20,
-            itemTextColor: "#999",
-            symbolSize: 12,
-            symbolShape: "circle",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemTextColor: "#000",
+    <div className={styles["graph-wrapper"]}>
+      <div className={styles.title}>
+        <h1>수치 그래프</h1>
+        <p>유형별 수치값을 확인할 수 있습니다.</p>
+      </div>
+      <div className={styles.graph}>
+        <ResponsiveRadar
+          data={resultData}
+          keys={["normal", "user"]}
+          indexBy="type"
+          valueFormat=">-.2f"
+          margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+          borderColor={{ from: "color" }}
+          gridLabelOffset={20}
+          dotSize={10}
+          dotColor={{ theme: "background" }}
+          dotBorderWidth={2}
+          colors={{ scheme: "paired" }}
+          blendMode="multiply"
+          motionConfig="wobbly"
+          legends={[
+            {
+              anchor: "top-left",
+              direction: "column",
+              translateX: -50,
+              translateY: -40,
+              itemWidth: 80,
+              itemHeight: 20,
+              itemTextColor: "#999",
+              symbolSize: 12,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
                 },
-              },
-            ],
-          },
-        ]}
-      />
+              ],
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
